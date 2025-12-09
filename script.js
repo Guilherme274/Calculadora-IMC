a4b811f500e4728509d276e2166131d857df3517
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2025-12-08 21:51:35 -0300

    Implementação de validação para entradas nulas

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 "use strict";
 
-
+const peso = document.getElementById("peso");
+const altura = document.getElementById("altura");
 const botaoCalcular = document.getElementById("main__btnCalcular");
 const resultado = document.getElementById("resultado");
+const resultado2 = document.getElementById("resultado2");
 
+function calculaIMC(peso, altura){
 
-function calculaIMC(){
-    const peso = parseFloat(document.getElementById("peso").value);
-    const altura = parseFloat(document.getElementById("altura").value);
-
-    const imc = peso/(altura * altura);
+    const pesoConvertido = parseFloat(peso.value);
+    const alturaConvertida = parseFloat(altura.value);
     
-    resultado.textContent = imc.toFixed(2);
+    if(Number.isNaN(pesoConvertido) || Number.isNaN(alturaConvertida)){
+        return "Os valores devem ser preenchidos";
+    }else{
+        return (pesoConvertido/(alturaConvertida * alturaConvertida)).toFixed(2);
+    }
 }
 
-
-
+function classificacaoIMC(imc){
+    if(typeof imc == "string")
+    {
+        return "";
+    }else{
+        if(imc < 18.5){
+            return "Magreza";
+        }else if(imc < 24.9){
+            return "Normal";
+        }else if(imc < 29.9){
+            return "SobrePeso";
+        }else if(imc < 39.9){
+            return "Obesidade";
+        }else{
+            return "Obesidade Grave";
+        }
+    }
+}
 
 botaoCalcular.addEventListener("click", 
-    calculaIMC);
+  function(){
+    const imc = calculaIMC(peso,altura);
+    const classificacao = classificacaoIMC(imc);
+
+    resultado.textContent = imc;
+    resultado2.textContent = classificacao;
+  });
+
+
+
 
 
 
